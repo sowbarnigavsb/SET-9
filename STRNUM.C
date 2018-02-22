73df434ac07affdf2c0c645a665175d45a5b9701
sowbarnigavsb <31504504+sowbarnigavsb@users.noreply.github.com>
2018-02-22 16:23:46 +0530

    Create STRNUM.C

---
+++ STRNUM.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+              int i;
+              char s[40];
+              scanf("%s",&str);
+              for(int i=1;s[i]!='\0';i++)
+              {
+              	if(s[i]>='0'&&s[i]<='9')
+                        printf("%c",s[i]);
+           
+              }
+           
+              
+	return 0;
+}
